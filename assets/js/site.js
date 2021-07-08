6512af4667dc37e81b1df745a85e46f0333993b3
Phil Ewels <phil.ewels@scilifelab.se>
2021-07-08 23:07:47 +0200

    Colourful side bar on the other side. Different font.

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,18 +1,29 @@
-function openDetails() {
-  document.getElementById("sidebar").style.left = "-400px";
-  document.getElementById("content").style.marginLeft = "0";
-  document.getElementById("content").style.width = "50%";
-  document.getElementById("content").style.marginRight = "50%";
-  document.getElementById("detail").style.right = "0";
-}
+var details_visible = null;
 
-function closeDetails() {
-  document.getElementById("sidebar").style.left = "0";
-  document.getElementById("content").style.marginLeft = "400px";
-  document.getElementById("content").style.width = "calc(100% - 400px)";
-  document.getElementById("content").style.marginRight = "0";
-  document.getElementById("detail").style.right = "-50%";
-}
+// Open the details pane
+document.querySelectorAll(".details_btn").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log(e);
+    document.getElementById("sidebar").style.left = "-400px";
+    document.getElementById("content").style.marginLeft = "0";
+    document.getElementById("content").style.width = "calc(50% - 30px)";
+    document.getElementById("content").style.marginRight = "50%";
+    document.getElementById("detail").style.right = "60px";
+  });
+});
+
+// Close the details pane
+document.querySelectorAll(".close_details_btn").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("sidebar").style.left = "0";
+    document.getElementById("content").style.marginLeft = "400px";
+    document.getElementById("content").style.width = "calc(100% - 460px)";
+    document.getElementById("content").style.marginRight = "0";
+    document.getElementById("detail").style.right = "calc(-50% - 30px)";
+  });
+});
 
 // Enable bootstrap tooltips
 var tooltipTriggerList = [].slice.call(
