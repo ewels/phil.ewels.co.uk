f3f316d5b06a5dbc055250a85cf5d73ee1d177b6
Phil Ewels <phil.ewels@seqera.io>
2022-11-28 12:04:51 +0100

    Favicon and site title

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "phil.ewels.co.uk";
+export const SITE_TITLE = "Phil Ewels";
 export const SITE_DESCRIPTION = "Personal website for Phil Ewels, a bioinformatician living in Stockholm, Sweden.";
