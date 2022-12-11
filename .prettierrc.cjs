7d5782ed5d049146f7a07b85bd43cf689d2e2346
Phil Ewels <phil.ewels@seqera.io>
2022-12-11 23:34:34 +0100

    Another prettier plugin

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -4,7 +4,12 @@ module.exports = {
   printWidth: 120,
 
   // pnpm support, for VSCode?
-  plugins: [require.resolve("prettier-plugin-astro"), require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-organize-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
   overrides: [
     {
       files: "*.astro",
