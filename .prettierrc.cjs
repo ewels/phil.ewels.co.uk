9c354fecc43f2887a389e2ee68212ee4585b653d
Phil Ewels <phil.ewels@seqera.io>
2022-12-12 00:12:16 +0100

    Build the contact page

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     require.resolve("prettier-plugin-astro"),
     require.resolve("prettier-plugin-organize-imports"),
-    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("prettier-plugin-tailwindcss"), // Must come last
   ],
   pluginSearchDirs: false,
   overrides: [
