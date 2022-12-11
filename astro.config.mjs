eeef09b6c25b68ccef25d921ee8ca48f4ab5b381
Phil Ewels <phil.ewels@seqera.io>
2022-12-11 23:35:55 +0100

    Run prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
