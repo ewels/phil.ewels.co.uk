419a036655e3ff6b1d4a1916a45c8e3194c3e660
Phil Ewels <phil.ewels@seqera.io>
2023-01-23 00:06:21 +0100

    Add Swup page transitions

---
+++ src/swup.js
@@ -0,0 +1,23 @@
+import SwupA11yPlugin from "@swup/a11y-plugin";
+import SwupGaPlugin from "@swup/ga-plugin";
+import SwupHeadPlugin from "@swup/head-plugin";
+import SwupPreloadPlugin from "@swup/preload-plugin";
+import SwupProgressPlugin from "@swup/progress-plugin";
+import Swup from "swup";
+
+const swup = new Swup({
+  plugins: [
+    new SwupA11yPlugin(),
+    new SwupHeadPlugin(),
+    new SwupPreloadPlugin(),
+    new SwupProgressPlugin(),
+    new SwupGaPlugin({
+      gaMeasurementId: "G-7F0L1P0KS1",
+    }),
+  ],
+});
+
+// Scroll page to top when we change content
+swup.on("contentReplaced", function () {
+  document.querySelector(".drawer-content").scrollTo(0, 0);
+});
