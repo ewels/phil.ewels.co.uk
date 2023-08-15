d145680d346a6d9e7bd2368a3856495ee4d5e1b1
Phil Ewels <phil.ewels@seqera.io>
2023-08-15 09:20:52 +0200

    Visual tweaks

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <button
-  class="rounded-full border dark:border-transparent bg-gray-100 dark:bg-gray-800 px-2 py-1"
+  class="rounded-full border dark:border-transparent bg-gray-100 dark:bg-zinc-800 px-2 py-1"
   title="Toggle between light and dark theme"
   on:click={() => toggleDarkMode()}
 >
